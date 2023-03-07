978528fc5c8062d76f54174d0d068d6e759cf8cf
Papon <papon.s@lawyerplusgroup.com>
2023-03-07 14:34:26 +0700

    [The Complete Android 12 & Kotlin Development Masterclass ][Drawing App] 127.Permissions Demo case permission camera and location  (Reviewed by Papon S.)

--- app/src/main/java/com/example/kiddrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kiddrawingapp/MainActivity.kt
@@ -1,21 +1,61 @@
 package com.example.kiddrawingapp
 
+
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.view.ContentInfoCompat
 import androidx.core.view.get
+
+
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView : DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
+    private var cameraResultLauncher : ActivityResultLauncher<String> =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()) {
+                isGranted ->
+                if (isGranted) {
+                    Toast.makeText(this,"Permission granted for camera.",Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this,"Permission denied for camera.",Toast.LENGTH_LONG).show()
+                }
+        }
+    private var cameraAndLocationResultLauncher : ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()) {
+                permissions ->
+                permissions.entries.forEach {
+                    val permissionName = it.key
+                    val isGranted = it.value
+
+                    if (isGranted) {
+                        if (permissionName == Manifest.permission.ACCESS_FINE_LOCATION) {
+                            Toast.makeText(this,"Permission granted for location.",Toast.LENGTH_LONG).show()
+                        } else if (permissionName == Manifest.permission.CAMERA) {
+                            Toast.makeText(this,"Permission granted for camera.",Toast.LENGTH_LONG).show()
+                        }
+                    } else {
+                        if (permissionName == Manifest.permission.ACCESS_FINE_LOCATION) {
+                            Toast.makeText(this,"Permission denied for location.",Toast.LENGTH_LONG).show()
+                        } else if (permissionName == Manifest.permission.CAMERA) {
+                            Toast.makeText(this,"Permission denied for camera.",Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +76,37 @@ class MainActivity : AppCompatActivity() {
         ib_brush.setOnClickListener {
             showBrushSizeChooserDialog()
         }
+
+        val ib_gallery : ImageButton = findViewById(R.id.ib_gallery)
+        ib_gallery.setOnClickListener {
+            checkOpenPermissionCamera()
+        }
+    }
+
+    private fun showRationaleDialog(
+        title: String,
+        message: String
+    ) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel") {dialog,_ ->
+                dialog.dismiss()
+            }
+        builder.create().show()
+    }
+
+    private fun checkOpenPermissionCamera() {
+        if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.M &&
+            shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+            showRationaleDialog("Permission requires camera access","Camera cannot used because Camera access is denied")
+        } else {
+//            cameraResultLauncher.launch(Manifest.permission.CAMERA)
+            cameraAndLocationResultLauncher.launch(
+                arrayOf(Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION)
+            )
+        }
+
     }
 
     private fun showBrushSizeChooserDialog() {
